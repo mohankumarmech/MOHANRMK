7ce5f0ce7fac8256d6cc631384f86485e1e6697a
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-01-21 16:41:20 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void maim()
+{
+char ch;
+clrscr();
+printf("enter the letter:",ch);
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+{
+printf("vowel");
+}
+else
+{
+printf("consonent");
+}
+getch();
+}
