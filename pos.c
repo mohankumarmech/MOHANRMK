9e624ee6342c25b1d6eaf41776f634f0b52da8b6
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-01-21 15:19:56 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value of a is %d",a);
+scanf("%d",&a);
+a>0?printf("positive"):printf("negative");
+getch()
+}
