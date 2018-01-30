48302905a367655a8edc79dd2d21031df4789088
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-01-30 16:30:00 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the value of a and b");
+scanf("%d%d",&a,&b);
+c=pow(a,b);
+printf("The value is %d",c);
+getch();
+}
