27bc1541bca81081e4476e154b0b0c8cb7d23b31
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-01-30 16:21:08 +0530

    Create numbers.c

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,num,sum=0;
+clrscr();
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+printf("sum of natural numbers upto %d",num,sum);
+}
+getch();
+}
