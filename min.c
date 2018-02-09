2763ac2641097aee5c057da51ebfa8cd4799bbe0
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-02-09 16:21:12 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,a[10],temp;
+    for(i=1;i<=10;i++)
+    {
+        printf("enter a value");
+        scanf("%d",&a[i]);
+    }
+    temp=a[0];
+    for(i=1;i<=10;i++)
+    {
+        if(a[0]>a[10])
+        {
+            temp=a[i];
+        }
+        ptintf("%d",temp);
+    }
+}
