822321754ac0b38e2060c1c5a765e426b98894b2
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-02-20 02:15:48 -0800

    Create intex.c

---
+++ intex.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[40],n,i;
+printf("enter n values");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter a value");
+scanf("%d",&a);
+}
+for(i=0;i<n;i++)
+{
+printf("%d,a[i]",a[i]);
+}
+}
+
