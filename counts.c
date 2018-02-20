b0f7ce37b6de027cfd3ea8d90f56ada6e48393b8
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-02-20 03:18:15 -0800

    Create counts.c

---
+++ counts.c
@@ -0,0 +1,14 @@
+#includde<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[50];
+int i,count=0;
+scanf("%s",&a[i]);
+for(i=0;s[i]!=='\n';i++)
+{
+if(a[i]==' ')
+count++;
+}
+printf("%d",count+1);
+}
