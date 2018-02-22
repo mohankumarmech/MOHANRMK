4304eecd74d03b21ded750c3aaadc66afb1e5b12
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-02-22 16:26:29 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a,chr;
+    int count=0;
+    printf("enter name");
+    scanf("%s",&a);
+    if(chr=='n')
+    {
+        count=count+1;
+    }
+    printf("%d",count);
+}
