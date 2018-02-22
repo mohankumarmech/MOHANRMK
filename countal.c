e6caa5e6ed5319842fa4b4536ba6d005ea18af26
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-02-22 16:07:27 +0530

    Create countal.c

---
+++ countal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char m[50];
+    int i,n;
+    printf("enter letters");
+    scanf("%s",&m[i]);
+    if((m[i]>='a'&&m[i]<='z')||(m[i]>='A'&&m[i]<='Z'))
+    {
+        n++;
+    }
+    i++;
+    printf("%d",n);
+}
