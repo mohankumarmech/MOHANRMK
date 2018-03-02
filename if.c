65aa90d35f423f9fb927b6ba3b86b1e957b84fa6
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-03-02 15:08:32 +0530

    Create if.c

---
+++ if.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    if(a>=1)
+    {
+        if(a<=10)
+                {printf("yes");}
+        else
+           { printf("no");}
+    }
+    
+}
