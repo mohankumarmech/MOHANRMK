7d96a1053e55f7f9aee6184c215f51fd19a07aa7
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-03-13 16:51:18 +0530

    Create wrd.c

---
+++ wrd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a;
+scanf("%d",a);
+switch(a)
+{
+case 1:
+printf("one");
+break;
+case 2:
+printf("two");
+break;
+case 3:
+printf("thre");
+break;
+case 4:
+printf("four");
+break;
+default:
+printf("");
+break;
+}
+}
