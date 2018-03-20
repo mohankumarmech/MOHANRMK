aad16c85237ceaeaac31d2238db0736723593a6a
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-03-20 16:49:38 +0530

    Create adddd.c

---
+++ adddd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[50],i,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+}
