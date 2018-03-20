1ff3a934ad2723cd977eabeb92cf228507d0c85f
mohankumarmech <35652809+mohankumarmech@users.noreply.github.com>
2018-03-20 16:01:33 +0530

    Create bet.c

---
+++ bet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=4,b=20,c;
+    scanf("%d",&c);
+    if((c>a)&&(c<b))
+    {
+        printf("yes");
+    
+    }
+    else
+    {
+        printf("no");
+    }
+}
